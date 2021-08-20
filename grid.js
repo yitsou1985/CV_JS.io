9f2b33eebd40522a9a202832217964f31c954726
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-08-20 16:41:35 +0300

    cv_js

---
+++ grid.js
@@ -0,0 +1,8 @@
+const openMenu = () =>{document.querySelector(".sidebar").classList.add("open")}
+const closeMenu= () =>{document.querySelector(".sidebar").classList.remove("open")}
+
+
+const main = 
+`
+<img  src="Suisse.jpeg" ></img>
+`
