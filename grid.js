81cfaf1612b0c334c7070e8b41b47994c8d0cf00
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-08-23 20:32:35 +0300

    avatar

--- grid.js
+++ grid.js
@@ -1,8 +1,30 @@
 const openMenu = () =>{document.querySelector(".sidebar").classList.add("open")}
 const closeMenu= () =>{document.querySelector(".sidebar").classList.remove("open")}
 
+const img = document.querySelector("#img");
 
-const main = 
-`
-<img  src="Suisse.jpeg" ></img>
-`
+   
+
+ 
+
+   const dblclick= img.addEventListener("dblclick",()=>
+    {
+        img.style.transform="translateY(200%)";
+        
+    })
+    
+
+   const click = img.addEventListener("click",()=>
+    {
+        img.style.transform="translateY(0%)";
+        
+    })
+
+
+if(dblclick){
+
+    dblclick
+}
+else if(click){
+    click
+}
