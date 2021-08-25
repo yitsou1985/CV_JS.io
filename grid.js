c390914565d9086bf7e4708e9b8c67e4f1f604db
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-08-25 20:10:11 +0300

    about&loginavatar

--- grid.js
+++ grid.js
@@ -1,30 +1,36 @@
 const openMenu = () =>{document.querySelector(".sidebar").classList.add("open")}
 const closeMenu= () =>{document.querySelector(".sidebar").classList.remove("open")}
 
-const img = document.querySelector("#img");
+// const img = document.querySelector("#img");
 
-   
-
- 
-
-   const dblclick= img.addEventListener("dblclick",()=>
-    {
-        img.style.transform="translateY(200%)";
+//    const dblclick= img.addEventListener("dblclick",()=>
+//     {
+//         img.style.transform="translateY(200%)";
         
-    })
+//     })
     
 
-   const click = img.addEventListener("click",()=>
-    {
-        img.style.transform="translateY(0%)";
+//    const click = img.addEventListener("click",()=>
+//     {
+//         img.style.transform="translateY(0%)";
         
-    })
+//     })
 
 
-if(dblclick){
+// if(dblclick){
 
-    dblclick
-}
-else if(click){
-    click
+//     dblclick
+// }
+// else if(click){
+//     click
+// }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
