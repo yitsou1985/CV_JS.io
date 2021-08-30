36a743ded0cde6b6600ce58f50c394176e6526be
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-08-30 12:33:05 +0300

    index.htm&&register.html

--- grid.js
+++ grid.js
@@ -1,29 +1,8 @@
+
+
 const openMenu = () =>{document.querySelector(".sidebar").classList.add("open")}
 const closeMenu= () =>{document.querySelector(".sidebar").classList.remove("open")}
 
-// const img = document.querySelector("#img");
-
-//    const dblclick= img.addEventListener("dblclick",()=>
-//     {
-//         img.style.transform="translateY(200%)";
-        
-//     })
-    
-
-//    const click = img.addEventListener("click",()=>
-//     {
-//         img.style.transform="translateY(0%)";
-        
-//     })
-
-
-// if(dblclick){
-
-//     dblclick
-// }
-// else if(click){
-//     click
-// }
 
 // Get the modal
 var modal = document.getElementById('id01');
@@ -34,3 +13,5 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
