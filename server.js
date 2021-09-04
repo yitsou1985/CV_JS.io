72d988134929d25f5873cfad47825b9e975bb9b7
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2021-09-04 22:10:37 +0300

    index.html<a>

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const request = require('request');
 
 
 const app = express();
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+"index.html")
-})
+});
 
-app.get('/home',function(req,res){
-    res.send(__dirname+"home.html")
-})
 
 app.listen(3000,function(){
     console.log("Server is running");
